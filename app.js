a195a1677ecd97f82cc9ebf8205052e38e4b86e8
geraldinas <geraldinagalvarez@gmail.com>
2024-09-19 19:18:06 -0400

    added background image

---
+++ app.js
@@ -0,0 +1,50 @@
+
+let imageCounter = null;
+let catImage = 'cat1.svg';
+let breadImage = 'bread-creature.png'
+let currentStamp = catImage;
+let stamps = [breadImage,catImage];
+
+function getRandomImage() {
+    return stamps[Math.floor(Math.random() * stamps.length)];
+}
+
+function createStampElement() {
+    const stampedElement = document.createElement('img');
+    stampedElement.className = 'stamped-image'; // image class for styling NOTE: make glow or give shadow
+    stampedElement.src = currentStamp;
+    currentStamp = 
+    imageCounter <= 6 ? catImage :
+    // imageCounter >= 7 ? breadImage :
+    // imageCounter >= 15 ? catImage :
+    currentStamp = getRandomImage();
+    imageCounter += 1
+    // console.log(imageCounter, stamp);
+    return stampedElement; 
+}
+
+// position the stamped image at the clicked coordinates && Center the Stamps
+function positionStamp(element, x, y) {
+    element.style.left = `${x - (element.width / 2)}px`; 
+    element.style.top = `${y - (element.height / 2)}px`; 
+}
+
+function handleMouseClick(event) {
+    let mouseX = event.clientX; 
+    let mouseY = event.clientY; 
+    // to handle space press
+    if (typeof mouseX === 'undefined'){
+        mouseX = Math.floor(Math.random()* window.innerWidth);
+        mouseY = Math.floor(Math.random()* window.innerWidth);
+        console.log('axis', mouseX, mouseY);
+    }
+    const stampedElement = createStampElement(); 
+    document.body.appendChild(stampedElement); 
+    positionStamp(stampedElement, mouseX, mouseY); // Image Position
+    // take into account space bar press
+
+}
+
+// Set up event listener for mouse down
+window.addEventListener('mousedown', handleMouseClick);
+window.addEventListener('keydown', handleMouseClick);
